842578bf5ecce68099cfe50e975bbc807964c777
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-02-24 19:49:31 +0900

    test on both colorful/monochrome environments

--- test.js
+++ test.js
@@ -4,39 +4,53 @@ var chalk = require('chalk');
 var sumUp = require('./');
 var test = require('tape');
 
-var cyan = chalk.cyan;
-var gray = chalk.gray;
-var supportsColor = chalk.supportsColor;
+var enabled = chalk.enabled;
 
-test('sumUp()', function(t) {
-  t.plan(10);
+var environment = '';
+/* istanbul ignore if */
+if (!chalk.supportsColor) {
+  environment = ' with non-color environment';
+}
+
+test('sumUp()' + environment, function(t) {
+  t.plan(11);
 
   t.equal(sumUp.name, 'sumUp', 'should have a function name.');
 
   t.equal(
-    sumUp({foo: 'bar'}), '',
+    sumUp({unsupportedObjectKey: 'bar'}),
+    '',
     'should return empty string when the object has no supported properties.'
   );
 
   t.equal(
-    sumUp({name: 'foo'}), cyan('foo'),
+    sumUp({name: 'foo'}),
+    chalk.cyan('foo'),
     'should return the name of the object with cyan color.'
   );
 
   t.equal(
     sumUp({version: '1.0.0', color: undefined}),
-    gray('v1.0.0'),
+    chalk.gray('v1.0.0'),
     'should return the version of the object with gray color.'
   );
 
   t.equal(
     sumUp({
       homepage: 'http://nodejs.org/',
-      description: 'foo',
+      description: 'foo'
+    }),
+    chalk.gray('http://nodejs.org/') + '\nfoo',
+    'should return the homepage URL and description of the object.'
+  );
+
+  t.equal(
+    sumUp({
+      name: 'å',
       color: true
     }),
-    gray('http://nodejs.org/') + '\n' + 'foo',
-    'should return the homepage URL and description of the object.'
+    chalk.styles.cyan.open + 'å' + chalk.styles.cyan.close,
+    'should explicitly add ANSI colors to the string when `color` option is enabled.'
   );
 
   t.equal(
@@ -47,31 +61,34 @@ test('sumUp()', function(t) {
       description: 'bar'
     }),
     [
-      cyan('foo') + ' ' + gray('v1.0.0'),
-      gray('http://nodejs.org/'),
+      chalk.cyan('foo') + ' ' + chalk.gray('v1.0.0'),
+      chalk.gray('http://nodejs.org/'),
       'bar'
     ].join('\n'),
     'should join all supported properties into a string.'
   );
 
   t.equal(
-    sumUp({version: '2.0', color: false}), 'v2.0',
+    sumUp({version: '2.0', color: false}),
+    'v2.0',
     'should omit colors from string when `color` option is disabled.'
   );
 
   t.strictEqual(
-    chalk.supportsColor,
-    supportsColor,
-    'should not modify `supportsColor` property of cached chalk module.'
+    chalk.enabled,
+    enabled,
+    'should not modify `enabled` property of cached chalk module.'
   );
 
   t.throws(
-    sumUp.bind(null), /TypeError.*must be an object\./,
+    sumUp.bind(null),
+    /TypeError.*must be an object\./,
     'should throw a type error when it takes no arguments.'
   );
 
   t.throws(
-    sumUp.bind(null, true),  /TypeError.*must be an object\./,
+    sumUp.bind(null, true),
+    /TypeError.*must be an object\./,
     'should throw a type error when the argument is not an object.'
   );
 });
