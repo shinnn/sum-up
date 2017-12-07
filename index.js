885ecf561605ba0d25942474622e6bd2b644e204
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-07 19:42:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7
    * Chalk#level = 0 is also needed to disable colors. https://github.com/chalk/chalk/commit/0827d3b6327dfd5e132589b1308703c9b4e127ef
    * istanbul -> nyc

--- index.js
+++ index.js
@@ -1,61 +1,57 @@
-/*!
- * sum-up | MIT (c) Shinnosuke Watanabe
- * https://github.com/shinnn/sum-up
-*/
 'use strict';
 
 const Chalk = require('chalk').constructor;
 const inspectWithKind = require('inspect-with-kind');
 
 module.exports = function sumUp(pkgData, options) {
-  if (!pkgData || Array.isArray(pkgData) || typeof pkgData !== 'object') {
-    throw new TypeError(
-      `Expected an object of package information \`{name: ... version: ..., description: ..., ...}\`, but got ${
-        inspectWithKind(pkgData)
-      }.`
-    );
-  }
+	if (!pkgData || Array.isArray(pkgData) || typeof pkgData !== 'object') {
+		throw new TypeError(`Expected an object of package information \`{name: ... version: ..., description: ..., ...}\`, but got ${
+			inspectWithKind(pkgData)
+		}.`);
+	}
 
-  if (options) {
-    if (Array.isArray(options) || typeof options !== 'object') {
-      throw new TypeError(`The second argument of sum-up must be a plain object or undefined, but got ${
-        inspectWithKind(options)
-      }.`);
-    }
-  } else {
-    options = {};
-  }
+	if (options) {
+		if (Array.isArray(options) || typeof options !== 'object') {
+			throw new TypeError(`The second argument of sum-up must be a plain object or undefined, but got ${
+				inspectWithKind(options)
+			}.`);
+		}
+	} else {
+		options = {};
+	}
 
-  if (options.color !== undefined && typeof options.color !== 'boolean') {
-    throw new TypeError(`Expected \`color\` option to be a Boolean value, but got ${
-      inspectWithKind(options.color)
-    }.`);
-  }
+	const chalkOption = {};
 
-  const chalkOption = {};
+	if (options.color === true) {
+		chalkOption.enabled = true;
+		chalkOption.level = 1;
+	} else if (options.color === false) {
+		chalkOption.enabled = false;
+		chalkOption.level = 0;
+	} else if (options.color !== undefined) {
+		throw new TypeError(`Expected \`color\` option to be a Boolean value, but got ${
+			inspectWithKind(options.color)
+		}.`);
+	}
 
-  if (options.color !== undefined) {
-    chalkOption.enabled = options.color;
-  }
+	const chalk = new Chalk(chalkOption);
+	const lines = [];
 
-  const chalk = new Chalk(chalkOption);
-  const lines = [];
+	const nameAndVersion = `${chalk.cyan(pkgData.name || '')}${
+		pkgData.version ? `${pkgData.name ? ' ' : ''}${chalk.gray(`v${pkgData.version}`)}` : ''
+	}`;
 
-  const nameAndVersion = `${chalk.cyan(pkgData.name || '')}${
-    pkgData.version ? `${pkgData.name ? ' ' : ''}${chalk.gray('v' + pkgData.version)}` : ''
-  }`;
+	if (nameAndVersion) {
+		lines.push(nameAndVersion);
+	}
 
-  if (nameAndVersion) {
-    lines.push(nameAndVersion);
-  }
+	if (pkgData.homepage) {
+		lines.push(chalk.gray(pkgData.homepage));
+	}
 
-  if (pkgData.homepage) {
-    lines.push(chalk.gray(pkgData.homepage));
-  }
+	if (pkgData.description) {
+		lines.push(pkgData.description);
+	}
 
-  if (pkgData.description) {
-    lines.push(pkgData.description);
-  }
-
-  return lines.join('\n');
+	return lines.join('\n');
 };
--- test.js
+++ test.js
@@ -7,90 +7,90 @@ const test = require('tape');
 
 const enabled = chalk.enabled;
 
-test(`sumUp()${' with non-color environment'.repeat(Number(chalk.supportsColor))}`, t => {
-  t.equal(sumUp.name, 'sumUp', 'should have a function name.');
+test(`sumUp()${' with non-color environment'.repeat(Number(!!chalk.supportsColor))}`, t => {
+	t.equal(sumUp.name, 'sumUp', 'should have a function name.');
 
-  t.equal(
-    sumUp({unsupportedObjectKey: 'bar'}),
-    '',
-    'should return empty string when the object has no supported properties.'
-  );
+	t.equal(
+		sumUp({unsupportedObjectKey: 'bar'}),
+		'',
+		'should return empty string when the object has no supported properties.'
+	);
 
-  t.equal(
-    sumUp({name: 'foo'}),
-    chalk.cyan('foo'),
-    'should return the name of the object with cyan color.'
-  );
+	t.equal(
+		sumUp({name: 'foo'}),
+		chalk.cyan('foo'),
+		'should return the name of the object with cyan color.'
+	);
 
-  t.equal(
-    sumUp({version: '1.0.0', color: undefined}),
-    chalk.gray('v1.0.0'),
-    'should return the version of the object with gray color.'
-  );
+	t.equal(
+		sumUp({version: '1.0.0', color: undefined}),
+		chalk.gray('v1.0.0'),
+		'should return the version of the object with gray color.'
+	);
 
-  t.equal(
-    sumUp({
-      homepage: 'http://nodejs.org/',
-      description: 'foo'
-    }),
-    chalk.gray('http://nodejs.org/') + '\nfoo',
-    'should return the homepage URL and description of the object.'
-  );
+	t.equal(
+		sumUp({
+			homepage: 'http://nodejs.org/',
+			description: 'foo'
+		}),
+		`${chalk.gray('http://nodejs.org/')}\nfoo`,
+		'should return the homepage URL and description of the object.'
+	);
 
-  t.equal(
-    sumUp({name: 'å'}, {color: true}),
-    `${cyan.open}å${cyan.close}`,
-    'should explicitly add ANSI colors to the string when `color` option is enabled.'
-  );
+	t.equal(
+		sumUp({name: 'å'}, {color: true}),
+		`${cyan.open}å${cyan.close}`,
+		'should explicitly add ANSI colors to the string when `color` option is enabled.'
+	);
 
-  t.equal(
-    sumUp({
-      name: 'foo',
-      version: '1.0.0',
-      homepage: 'http://nodejs.org/',
-      description: 'bar'
-    }),
-    `${chalk.cyan('foo')} ${chalk.gray('v1.0.0')}
+	t.equal(
+		sumUp({
+			name: 'foo',
+			version: '1.0.0',
+			homepage: 'http://nodejs.org/',
+			description: 'bar'
+		}),
+		`${chalk.cyan('foo')} ${chalk.gray('v1.0.0')}
 ${chalk.gray('http://nodejs.org/')}
 bar`,
-    'should join all supported properties into a string.'
-  );
+		'should join all supported properties into a string.'
+	);
 
-  t.equal(
-    sumUp({version: '2.0'}, {color: false}),
-    'v2.0',
-    'should omit colors from string when `color` option is disabled.'
-  );
+	t.equal(
+		sumUp({version: '2.0'}, {color: false}),
+		'v2.0',
+		'should omit colors from string when `color` option is disabled.'
+	);
 
-  t.equal(
-    chalk.enabled,
-    enabled,
-    'should not modify `enabled` property of cached chalk module.'
-  );
+	t.equal(
+		chalk.enabled,
+		enabled,
+		'should not modify `enabled` property of cached chalk module.'
+	);
 
-  t.throws(
-    () => sumUp(),
-    /TypeError.*Expected an object of package information `{name: \.{3} version: \.{3}, description: \.{3}, \.{3}}`/,
-    'should throw a type error when it takes no arguments.'
-  );
+	t.throws(
+		() => sumUp(),
+		/TypeError.*Expected an object of package information `{name: \.{3} version: \.{3}, description: \.{3}, \.{3}}`/,
+		'should throw a type error when it takes no arguments.'
+	);
 
-  t.throws(
-    () => sumUp(['A', 'B']),
-    /TypeError.*, but got \[ 'A', 'B' ] \(array\)\./,
-    'should throw a type error when the argument is not a plain object.'
-  );
+	t.throws(
+		() => sumUp(['A', 'B']),
+		/TypeError.*, but got \[ 'A', 'B' ] \(array\)\./,
+		'should throw a type error when the argument is not a plain object.'
+	);
 
-  t.throws(
-    () => sumUp({version: '0.0.0'}, Infinity),
-    /TypeError.*The second argument of sum-up must be a plain object or undefined, but got Infinity \(number\)\./,
-    'should throw a type error when the second argument is not a plain object.'
-  );
+	t.throws(
+		() => sumUp({version: '0.0.0'}, Infinity),
+		/TypeError.*The second argument of sum-up must be a plain object or undefined, but got Infinity \(number\)\./,
+		'should throw a type error when the second argument is not a plain object.'
+	);
 
-  t.throws(
-    () => sumUp({version: '1.0.0'}, {color: new Set(['true'])}),
-    /TypeError.*Expected `color` option to be a Boolean value, but got Set { 'true' }\./,
-    'should throw a type error when `color` option is not a Boolean value.'
-  );
+	t.throws(
+		() => sumUp({version: '1.0.0'}, {color: new Set(['true'])}),
+		/TypeError.*Expected `color` option to be a Boolean value, but got Set { 'true' }\./,
+		'should throw a type error when `color` option is not a Boolean value.'
+	);
 
-  t.end();
+	t.end();
 });
